0417c3926e06bd69d23a6dc4b609928ff9a7baf3
Thomas Gustafson <gus.duckfan@gmail.com>
2024-07-10 17:14:12 -0700

    Adds github and linked in links

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-import "./style.css"
+import "./style.css";
+
+let projects = [];
+
+
+//add project cards
+
+const githubAboutLink = document.querySelector("#about-links a:nth-child(2)");
+const linkedInAboutLink = document.querySelector("#about-links a:nth-child(3)");
+const githubFooterLink = document.querySelector(
+  ".contact-links a:nth-child(1)",
+);
+const linkedInFooterLink = document.querySelector(
+  ".contact-links a:nth-child(2)",
+);
+
+githubAboutLink.addEventListener("click", (event) => {
+  window.open("https://www.github.com/tgustafson2");
+});
+
+linkedInAboutLink.addEventListener("click", (event) => {
+  window.open("https://www.linkedin.com/in/thomas-gustafson-825a921ba//");
+});
+
+githubFooterLink.addEventListener("click", (event) => {
+  window.open("https://www.github.com/tgustafson2");
+});
+
+linkedInFooterLink.addEventListener("click", (event) => {
+  window.open("https://www.linkedin.com/in/thomas-gustafson-825a921ba//");
+});
